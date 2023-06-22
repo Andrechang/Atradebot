cde2319051f7a6eb2dcc89881a6cd490c83ec7a2
Andrechang <andrechang67@gmail.com>
2023-06-22 08:59:11 -0700

    add print alloc

--- src/atradebot/backtest.py
+++ src/atradebot/backtest.py
@@ -9,6 +9,7 @@ from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 from pypfopt.efficient_frontier import EfficientFrontier
 
 pd.options.mode.chained_assignment = None
+DATE_FORMAT = "%Y-%m-%d"
 
 class PortfolioBacktester:
     def __init__(self, initial_capital, data, stocks, start_date):
@@ -17,6 +18,8 @@ class PortfolioBacktester:
         self.portfolio = pd.DataFrame(0, index=self.data.index, columns=['Cash', 'Total'] + stocks)
         self.stocks = stocks
         self.start_date = start_date
+        self.activity = [] #list of (date, allocation)
+
 
     def run_backtest(self, strategy):
         # Initialize portfolio with initial capital
@@ -27,9 +30,10 @@ class PortfolioBacktester:
             # Retrieve current date and price
             date = self.data.index[i]
             # Call strategy to determine portfolio allocation
-            allocation = strategy.generate_allocation(date) # dict{stock: alloc}
+            allocation = strategy.generate_allocation(date) # dict{stock: alloc} 
             holding = 0
             cash = self.portfolio['Cash'][i]
+            action = False
             for stock in self.stocks:
                 if len(self.data['Close'].columns) > 1: #track multiple stocks
                     price = self.data['Close'][stock][i]
@@ -37,15 +41,18 @@ class PortfolioBacktester:
                     price = self.data['Close'][i]
 
                 # Update portfolio holdings and cash based on allocation
-                if stock in allocation.keys() and cash > price*allocation[stock]: #buy/sell
+                if stock in allocation.keys() and cash > price*allocation[stock] and allocation[stock] != 0: #buy/sell
                     cash -= price*allocation[stock]
                     self.portfolio[stock][i+1] = self.portfolio[stock][i] + allocation[stock] 
+                    action = True
                 else: #hold
                     self.portfolio[stock][i+1] = self.portfolio[stock][i]
                 
                 holding += price*self.portfolio[stock][i+1]
                 
             # Calculate total portfolio value
+            if action:
+                self.activity.append((date, allocation))
             self.portfolio['Cash'][i+1] = cash
             self.portfolio['Total'][i+1] = holding + cash
 
@@ -133,5 +140,11 @@ if __name__ == "__main__":
     idx = data.index.get_loc(start_date)
     data_spy = data['Close']['SPY'][idx:]
     data_my = backtester.portfolio['Total'][idx:]
-    plot_cmp({"SPY":data_spy/data_spy[0], #normalize gains
-            "MyPort":data_my/INIT_CAPITAL}, show=True)
+    plt = plot_cmp({"SPY":data_spy/data_spy[0], #normalize gains
+            "MyPort":data_my/INIT_CAPITAL}, show=False)
+
+    for date, alloct in backtester.activity:
+        idx = data.index.get_loc(date)
+        plt.scatter(date, backtester.portfolio['Total'][idx]/INIT_CAPITAL, color='blue')
+        print(datetime.strptime(date, DATE_FORMAT), alloct)
+    plt.show()
