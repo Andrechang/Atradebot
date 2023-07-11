38369eddfdefe5bc5e95d22683d515353e6cd215
Eugenio Culurciello <culurciello@gmail.com>
2023-07-11 11:52:40 -0400

    moved to utils and strategies and cleaned main

--- src/atradebot/main.py
+++ src/atradebot/main.py
@@ -1,43 +1,16 @@
+
 import pandas as pd
-import tweepy
+# import tweepy
 from argparse import ArgumentParser
 import os
 from datetime import date, datetime, timedelta
-import yfinance as yf
-import shutil 
-import math
-import yaml
-import requests
-from bs4 import BeautifulSoup
-import trafilatura
-from trafilatura.settings import use_config
+import shutil
+from utils import get_config, is_business_day, business_days
 
-from nltk import tokenize
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import pipeline
-from dateutil.relativedelta import relativedelta
-from tqdm import tqdm 
+# from nltk import tokenize
+# from transformers import BertTokenizer, BertForSequenceClassification
+# from transformers import pipeline
 
-trafilatura_config = use_config()
-trafilatura_config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
-
-DATE_FORMAT = "%Y-%m-%d"
-
-def pd_append(df, dict_d):
-    return pd.concat([df, pd.DataFrame.from_records([dict_d])])
-
-def get_price(stock):
-    ticker = yf.Ticker(stock).info
-    return ticker['regularMarketOpen']
-
-# holdings: Name, Qnt, UCost (unit cost), BaseCost, Price (current price), Value (current Value), LongGain (Qnt), ShortGain (Qnt)
-# activity: Name, type (buy/sell), TB (time bought), Qnt, Proceed
-# balance: Time, Cash, Stock, Total
-# news: Time, Name, Text, Score, Link
-def get_config(cfg_file):
-    with open(cfg_file, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    return cfg
 
 def get_arg(raw_args=None):
     parser = ArgumentParser(description="parameters")
@@ -50,99 +23,8 @@ def get_arg(raw_args=None):
     config.update(vars(args))
     return args, config
 
-# run financial sentiment analysis model
-def get_sentiment(text, model, max_length=512):
-    sentences = tokenize.sent_tokenize(text)
-    # truncate sentences that are too long
-    for i, s in enumerate(sentences):
-        if len(s) > max_length:
-            sentences[i] = sentences[i][:max_length]
 
-    sentiment = model(sentences)
-    sum, neutrals = 0, 0
-    if len(sentiment) > 0:
-        for r in sentiment: 
-            sum += (r["label"] == "Positive")
-            neutrals += (r["label"] == "Neutral")
-
-        den = len(sentiment)-neutrals
-        sentiment = sum/den if den > 0 else 1.0 # as all neutral
-    return sentiment
-
-def is_business_day(date):
-    return bool(len(pd.bdate_range(date, date)))
-
-#add/subtract business days
-def business_days(start_date, num_days):
-    current_date = start_date
-    business_days_added = 0
-    while business_days_added < abs(num_days):
-        if num_days < 0:
-            current_date -= timedelta(days=1)
-        else:
-            current_date += timedelta(days=1)
-        if current_date.weekday() < 5:  # Monday to Friday
-            business_days_added += 1
-    return current_date
-
-def get_forecast(stock, date):
-    """get forecast for stock on date
-    Args:
-        stock (string): stock id
-        date (string): date format 'Feb 2, 2019' "%b %d, %Y"
-
-    Returns:
-        forecast: forecast list [1mon, 5mon, 1 yr], higher than 1. percent increase, lower than 1. percent decrease
-    """    
-    s = datetime.strptime(date, "%b %d, %Y")
-    e = business_days(s, +3)
-    data = yf.Ticker(stock)
-    hdata = data.history(start=s.strftime("%Y-%m-%d"),  end=e.strftime("%Y-%m-%d"))
-    price = hdata['Close'].mean()
-
-    forecast = [0, 0, 0]
-    add_days = [21, 5*21, 12*21] #add business days
-    for idx, adays in enumerate(add_days):
-        s = business_days(s, adays)#look into future
-        e = business_days(s, +3)
-        hdata = data.history(start=s.strftime("%Y-%m-%d"),  end=e.strftime("%Y-%m-%d"))
-        forecast[idx] = hdata['Close'].mean()/price
-    
-    return forecast
-
-# collect google search text 
-def get_google_news(stock, num_results=10, time_period=[]):
-    # time_period=['2019-06-28' (start_time), '2019-06-29' (end_time)]
-    query = stock
-    headers = {
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
-        }
-    if time_period:
-        query += f"+before%3A{time_period[1]}+after%3A{time_period[0]}" # add time range     
-    search_req = "https://www.google.com/search?q="+query+"&gl=us&tbm=nws&num="+str(num_results)+""
-    #https://developers.google.com/custom-search/docs/xml_results#WebSearch_Request_Format
-    news_results = []
-    # get webpage content
-    response = requests.get(search_req, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    for el in soup.select("div.SoaBEf"):
-        sublink = el.find("a")["href"]
-        downloaded = trafilatura.fetch_url(sublink)
-        html_text = trafilatura.extract(downloaded, config=trafilatura_config)
-        if html_text:
-            news_results.append(
-                {
-                    "link": el.find("a")["href"],
-                    "title": el.select_one("div.MBeuO").get_text(),
-                    "snippet": el.select_one(".GI74Re").get_text(),
-                    "date": el.select_one(".LfVVr").get_text(),
-                    "source": el.select_one(".NUnG9d span").get_text(),
-                    "text": html_text,
-                    "stock": stock,
-                }
-            )
-    return news_results, search_req, soup
+DATE_FORMAT = "%Y-%m-%d"
 
 class TradingBot:
     def __init__(self, cfg):
@@ -154,7 +36,7 @@ class TradingBot:
         self.stats = {} #stats for stocks: dict {stock: history data}
         self.stock_rank = {} #ranked stocks: dict{stock: {decision: buy/sell,  news: [ranked news], info: "analysis" } }
         
-        self.init_model()#init sentiment analysis model
+        # self.init_model()#init sentiment analysis model
 
     # PROFILE SAVING===================================================================================================
     
@@ -206,23 +88,23 @@ class TradingBot:
 
     # INFO GATHERING===================================================================================================
     # Authenticate to Twitter
-    def init_tweet(self):
-        auth = tweepy.OAuthHandler("UkIxHV1myPKpxf2bEwd1WCmM1", "ifh3rDZDHG8C4tu2JsgtgDbQGD77WgkdgL5t1P7zyHp3c9Dero")
-        auth.set_access_token("177934931-wioPKK09BQF5jNLKyUVSvH4GlJRh0LCZqzorXHk5", "QnmDsrBkdbSTxpi7LFz4MtdMB83Rgt8B7vmIF5KsuES0Z")
-        # Create API object
-        self.api = tweepy.API(auth)
-        try:
-            self.api.verify_credentials()
-            print("Authentication ok")
-        except:
-            print("Error in authentication")
+    # def init_tweet(self):
+    #     auth = tweepy.OAuthHandler("UkIxHV1myPKpxf2bEwd1WCmM1", "ifh3rDZDHG8C4tu2JsgtgDbQGD77WgkdgL5t1P7zyHp3c9Dero")
+    #     auth.set_access_token("177934931-wioPKK09BQF5jNLKyUVSvH4GlJRh0LCZqzorXHk5", "QnmDsrBkdbSTxpi7LFz4MtdMB83Rgt8B7vmIF5KsuES0Z")
+    #     # Create API object
+    #     self.api = tweepy.API(auth)
+    #     try:
+    #         self.api.verify_credentials()
+    #         print("Authentication ok")
+    #     except:
+    #         print("Error in authentication")
     
-    # get NLP model to analyze sentiment
-    def init_model(self):
-        finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3)
-        tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
-        self.sentiment_analyzer = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer)
 
+    # get NLP model to analyze sentiment
+    # def init_model(self):
+    #     finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3)
+    #     tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+    #     self.sentiment_analyzer = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer)
     
 
     # get the news from: google, TODO yfinance, twitter, 
@@ -350,7 +232,6 @@ class TradingBot:
         total_stock = self.holdings['Value'].sum()
         self.balance.at[0, 'Stock'] = total_stock
         self.balance.at[0, 'Time'] = str(datetime.now().strftime(DATE_FORMAT))
-
 
 
 
---
+++ src/atradebot/strategies.py
@@ -0,0 +1,204 @@
+
+from datetime import date, datetime
+from pypfopt import risk_models, expected_returns
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+from pypfopt.efficient_frontier import EfficientFrontier
+
+from utils import business_days, get_google_news
+from fin_train import get_model, generate_prompt
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Define a simple strategy that allocates 50% of the portfolio on the first day
+def max_sharpe_allocation(data, amount_invest):
+    mu = expected_returns.mean_historical_return(data)
+    S = risk_models.sample_cov(data)
+    ef = EfficientFrontier(mu, S)
+    raw_weights = ef.max_sharpe()
+    cleaned_weights = ef.clean_weights()
+    latest_prices = get_latest_prices(data)
+    da = DiscreteAllocation(cleaned_weights, latest_prices, total_portfolio_value=amount_invest)
+    allocation, leftover = da.greedy_portfolio()
+    return allocation, leftover
+
+
+class SimpleStrategy:
+    def __init__(self, start_date, end_date, data, stocks, cash=10000):
+        start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+        self.prev_date = start_date  #previous date for rebalance
+        self.stocks = {i: 0 for i in stocks}
+        
+        self.cash = [cash*0.5, cash*0.3, cash*0.1, cash*0.1] #amount to invest in each rebalance
+        self.cash_idx = 0
+
+        self.start_date = start_date
+        self.end_date = end_date
+        delta = end_date - start_date
+        self.days_interval = delta.days/len(self.cash) #rebalance 4 times
+        self.data = data['Adj Close']
+
+    def generate_allocation(self, date, portfolio):
+        """Generate allocation based on the date
+        Args:
+            date (pandas.Timestamp): date to generate allocation
+
+        Returns:
+            dict{"stock": number of stocks to buy/sell }: allocation for each stock
+        """
+        delta = date.date() - self.prev_date
+        idx = self.data.index.get_loc(str(date.date()))
+        if date.date() == self.prev_date: #first day
+            allocation, leftover = max_sharpe_allocation(self.data[0:idx], amount_invest=self.cash[self.cash_idx])
+            self.cash_idx += 1                        
+            return allocation
+        elif delta.days > self.days_interval: #rebalance 
+            allocation, leftover = max_sharpe_allocation(self.data[0:idx], amount_invest=self.cash[self.cash_idx])
+            self.prev_date = date.date()
+            self.cash_idx += 1
+            return allocation
+        else:
+            return self.stocks
+
+
+
+
+class FinForecastStrategy:
+    def __init__(self, start_date, end_date, data, stocks, cash=10000, model_id="achang/fin_forecast"):
+        """
+        model:
+        data: achang/stock_forecast
+        input: 
+            Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+                ## Instruction: what is the forecast for ... 
+                ## Input: date, news title, news snippet
+        output:
+                ## Response: forecast percentage change for 1mon, 5mon, 1 yr
+        """
+
+        start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+        self.prev_date = start_date #previous date for rebalance
+        self.stocks = {i: 0 for i in stocks}
+        
+        self.cash = [cash*0.5, cash*0.3, cash*0.1, cash*0.1] #amount to invest in each rebalance
+        self.cash_idx = 0
+
+        self.start_date = start_date
+        self.end_date = end_date
+        delta = end_date - start_date
+        self.days_interval = delta.days/len(self.cash) #rebalance 4 times
+        self.data = data['Adj Close']
+
+        self.model, self.tokenizer = get_model(model_id)
+        self.model.to(device)
+
+    def model_run(self, date, num_news=5):
+        """get news before date and forecast using model
+
+        Args:
+            date (pandas.Timestamp): date = pd.Timestamp("2021-07-22")
+            num_news (int, optional): number of news to get. Defaults to 5.
+
+        Returns:
+            dict{stock: list of pred }: prediction for each stock. Prediction format [1 mon, 5 mon, 1 yr]
+        """
+
+        end = date.date()
+        start = business_days(end, -5)
+        all_stocks = {}
+        for stock in self.stocks:
+            news, _, _ = get_google_news(stock=stock, num_results=num_news, time_period=[start, end])
+            assert len(news) > 0, "no news found, google search blocked error"
+            all_pred = []
+            for new in news:
+                in_dict = {'instruction': f"what is the forecast for {new['stock']}", 
+                        'input':f"{new['date']} {new['title']} {new['snippet']}"}
+                prompt = generate_prompt(in_dict, mode='eval')
+                in_data = self.tokenizer(prompt, return_tensors="pt", truncation=True, padding="max_length")
+                in_data['input_ids'] = in_data['input_ids'].to(device)
+                with torch.cuda.amp.autocast():
+                    outputs = self.model.generate(input_ids = in_data['input_ids'], 
+                        attention_mask = in_data['attention_mask'],
+                        max_new_tokens=128,
+                        pad_token_id= self.tokenizer.eos_token_id,
+                        eos_token_id= self.tokenizer.eos_token_id
+                    )
+
+                response = get_response(outputs[0].cpu().numpy(), self.tokenizer)
+                pred = re.findall(r"[-+]?(?:\d*\.*\d+)", response)
+                print(f"{new['stock']} forecast: {response} \n {pred}")
+                if len(pred) > 3:
+                    pred = pred[:3]
+                if len(pred) < 3:
+                    continue
+                
+                pred = [eval(i) for i in pred] #convert to str to float
+                all_pred.append(pred)
+
+            #average forecast
+            avg_pred = np.mean(np.array(all_pred), axis=0)
+            all_stocks[stock] = avg_pred
+        return all_stocks
+
+    def model_allocation(self, date, amount_invest, prices, portfolio, sell_mode=False):
+        """get allocation for each stock based on model predictions
+        Args:
+            date (pandas.Timestamp): date = pd.Timestamp("2021-07-22")
+            amount_invest (int): amount to invest
+            prices (pandas.Series): list of stock prices
+            portfolio (dict{stock: number of shares}): current portfolio
+            sell_mode (bool, optional): whether to sell stocks. Defaults to False.
+        Returns:
+            dict{stock: number to buy/sell}: allocation for each stock
+        """        
+        all_stocks = self.model_run(date)
+        #pick top increasing forecast
+        future_mode = 0 #choose timeline 1mon, 5mon, 1yr
+        alloc = sorted(all_stocks.items(), key=lambda x: x[1][future_mode], reverse=True) #most increase first 
+        # alloc: tuple(stock, forecast)
+        weights = [0.6, 0.3, 0.1] #weight for each stock
+        amounts = [int(amount_invest * weight) for weight in weights]
+        allocation = {i[0]: int(amounts[idx]/prices[i[0]]) for idx, i in enumerate(alloc[:3])}#get top3 stocks
+
+        #get top stocks to sell
+        if sell_mode:
+            alloc_sell = sorted(all_stocks.items(), key=lambda x: x[1][future_mode], reverse=False) #most decrease first 
+            weights_sell = 0.5 #weight to sell holdings 
+            for sell in alloc_sell:
+                if portfolio[sell[0]][date] > 0:
+                    amounts_sell = int(portfolio[sell[0]][date]*weights_sell)
+                    allocation[sell[0]] = -amounts_sell
+                    break
+
+        leftover = amount_invest - sum([prices[i[0]]*allocation[i[0]] for i in alloc[:3]])
+        return allocation, leftover
+        #TODO balance with max_sharpe_allocation
+        
+
+    def generate_allocation(self, date, portfolio):
+        """generate allocation for each stock using average cost investment method
+        Args:
+            date (pandas.Timestamp): date = pd.Timestamp("2021-07-22")
+
+        Returns:
+            dict{stock: number to buy/sell}: allocation for each stock
+        """        
+        delta = date.date() - self.prev_date
+        idx = self.data.index.get_loc(str(date.date()))
+        if date.date() == self.prev_date: #first day
+            # allocation, leftover = max_sharpe_allocation(self.data[0:idx], amount_invest=self.cash[self.cash_idx])
+            allocation, leftover = self.model_allocation(date, amount_invest=self.cash[self.cash_idx], 
+                prices=self.data.iloc[idx], portfolio=portfolio, sell_mode=False)
+            self.cash_idx += 1                        
+            return allocation
+        elif delta.days > self.days_interval: #rebalance 
+            # allocation, leftover = max_sharpe_allocation(self.data[0:idx], amount_invest=self.cash[self.cash_idx])
+            allocation, leftover = self.model_allocation(date, amount_invest=self.cash[self.cash_idx], 
+                prices=self.data.iloc[idx], portfolio=portfolio, sell_mode=True)
+            self.prev_date = date.date()
+            self.cash_idx += 1
+            return allocation
+        else:
+            return self.stocks
