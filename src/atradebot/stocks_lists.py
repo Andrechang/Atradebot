1b77a24d35bb1edfd3ab5abc4c2139892b7df454
Eugenio Culurciello <culurciello@gmail.com>
2023-07-14 16:20:10 -0400

    Create stocks_lists.py

---
+++ src/atradebot/stocks_lists.py
@@ -0,0 +1,3 @@
+stocks_list_renewable = ['FSLR', 'DQ', 'CWEN','BYD', 'ALB', 'LTHM', 'LAC', 'TSLA', 'RIVN'] # solar, battery, ecar
+stock_list_tech = ['AAPL', 'MSFT', 'NVDA', 'AVGO', 'AMD', 'ADBE', 'AMAT', 'GOOGL'] # tech
+stocks_list_rareearth = ['NYSE:MP', 'TSX:EFR', 'TSX:NB', 'TSX:ARA', 'TSXV:UCU', 'ASX:LYC', 'ASX:ILU', 'ASX:ARU'] # rare earth
