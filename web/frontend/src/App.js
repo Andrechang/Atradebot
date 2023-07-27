d01b4b0058d00d2f5e6c6a5e825a4fc9a0ec77ff
Ansh Sarin <63926935+anshsarin13@users.noreply.github.com>
2023-07-27 14:45:45 +0000

    made changes

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React  from "react";
 import "./App.css";
 
 function App() {
 
-	const [data, setdata] = useState({
-		name: "",
-		age: 0,
-		programming: "",
-	});
+	// const [data, setdata] = useState({
+	// 	name: "",
+	// 	age: 0,
+	// 	programming: "",
+	// });
 
 
-	useEffect(() => {
+	// useEffect(() => {
 
-		fetch("/home").then((res) =>
-			res.json().then((data) => {
-				setdata({
-					name: data.Name,
-					age: data.Age,
-					date: data.Date,
-					programming: data.programming,
-				});
-			})
-		);
+	// 	fetch("/home").then((res) =>
+	// 		res.json().then((data) => {
+	// 			setdata({
+	// 				name: data.Name,
+	// 				age: data.Age,
+	// 				date: data.Date,
+	// 				programming: data.programming,
+	// 			});
+	// 		})
+	// 	);
 
-		fetch("/signup").then((res) =>
-			res.json().then((data) => {
-				setdata({
-					name: data.Name,
-					age: data.Age,
-					date: data.Date,
-					programming: data.programming,
-				});
-			})
-		);
+	// 	fetch("/signup").then((res) =>
+	// 		res.json().then((data) => {
+	// 			setdata({
+	// 				name: data.Name,
+	// 				age: data.Age,
+	// 				date: data.Date,
+	// 				programming: data.programming,
+	// 			});
+	// 		})
+	// 	);
 
-	}, []);
+	// }, []);
 
 	return (
 		<div className="App">
-			<header className="App-header">
+			{/* <header className="App-header">
 				<h1>React and flask</h1>
-				{/* Calling a data from setdata for showing */}
+				Calling a data from setdata for showing
 				<p>{data.name}</p>
 				<p>{data.age}</p>
 				<p>{data.programming}</p>
 
-			</header>
+			</header> */}
 		</div>
 	);
 }
