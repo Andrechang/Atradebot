40629a9a28f52d958c36bd0131ac30549babe1a6
Ansh Sarin <63926935+anshsarin13@users.noreply.github.com>
2023-07-27 19:21:05 +0000

    commit

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,69 +1,28 @@
-import React  from "react";
+import React from "react";
 import "./App.css";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Signup from './components/Signup';
 
-import{
-	BrowserRouter,Route,Routes
-  } from "react-router-dom"
-  
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
-
-	// const [data, setdata] = useState({
-	// 	name: "",
-	// 	age: 0,
-	// 	programming: "",
-	// });
-
-
-	// useEffect(() => {
-
-	// 	fetch("/home").then((res) =>
-	// 		res.json().then((data) => {
-	// 			setdata({
-	// 				name: data.Name,
-	// 				age: data.Age,
-	// 				date: data.Date,
-	// 				programming: data.programming,
-	// 			});
-	// 		})
-	// 	);
-
-	// 	fetch("/signup").then((res) =>
-	// 		res.json().then((data) => {
-	// 			setdata({
-	// 				name: data.Name,
-	// 				age: data.Age,
-	// 				date: data.Date,
-	// 				programming: data.programming,
-	// 			});
-	// 		})
-	// 	);
-
-	// }, []);
-
-	return (
-		<div className="App">
-			<BrowserRouter>
-			<Navbar/>
-			{/* <header className="App-header">
-				<h1>React and flask</h1>
-				Calling a data from setdata for showing
-				<p>{data.name}</p>
-				<p>{data.age}</p>
-				<p>{data.programming}</p>
-
-			</header> */}
-			<Routes>
-				<Route exact path='/home' element={<Home/>}/>
-				<Route exact path='/signup' element={<Signup/>}/>
-
-			</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Define your routes inside the <Routes> component */}
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- web/frontend/src/components/Navbar.js
+++ web/frontend/src/components/Navbar.js
@@ -7,17 +7,17 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/home">Learn2Grow</Link>
+    <Link className="navbar-brand" to="/">Learn2Grow</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link " aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/home">Link</Link>
+          <Link className="nav-link" to="/">Link</Link>
         </li>
         <li className="nav-item dropdown">
           <Link className="nav-link dropdown-toggle" to="/features" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -37,9 +37,9 @@ export default function Navbar() {
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-        <button className="btn btn-outline-success" type="submit">
-            <Link to='/signup' className='signup'></Link>
-        Sign Up</button>
+        <Link to="/signup" className="btn btn-outline-success signup">
+              Sign Up
+            </Link>
 
       </form>
     </div>
