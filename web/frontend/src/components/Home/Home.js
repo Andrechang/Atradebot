950381a4e2c28c70e39580c496080f778e1e5ef5
Ansh Sarin <63926935+anshsarin13@users.noreply.github.com>
2023-08-11 20:08:07 +0000

    commit

--- web/frontend/src/components/Home/Home.js
+++ web/frontend/src/components/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import Slider from './Slider';
+import './Home.css';
 export default function Home() {
   
   return (
-    <div>
-      <Slider/>
+    <div className='body'>
+      <div className="box">
+        <h2 className='home-h2'>Welcome To</h2>
+        <h1 className='home-h1'>WISEBUCK.AI</h1>
+        <p className='home-h31'><i>The personal assistant for your</i></p>
+        <p className='home-h32'><i>Investment Journey</i></p>
+        <button className='btn btn-primary home-btn1'>Get Started</button> <br />
+        <button className='btn btn-primary home-btn2'>Login</button>
+
+      </div>
+
       
     </div>
   )
--- web/frontend/src/components/Navbar.js
+++ web/frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">Learn2Grow</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
